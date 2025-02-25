19f721e174d0365eb4efe86ab1883373be16a082
dhanya <dhanya695501@gmail.com>
2025-02-25 08:54:59 +0000

    crm project add

---
+++ crm/authentication/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import  AbstractUser
+# Create your models here.
+class Rolechoice(models.TextChoices):
+
+    ADMIN='ADMIN','ADMIN'
+
+    STUDENT='STUDENT','STUDENT'
+
+    ACADEMIC_COUNCELLOR='ACADEMIC COUNCELLOR','ACADEMIC_COUNCELLOR'
+
+    TRAINER='TRAINER','TRAINER'
+
+    SALES='SALES','SALES'
+
+class Profile(AbstractUser):
+    
+
+    role=models.CharField(max_length=30,choices=Rolechoice.choices)
+
+    def __str__(self):
+
+        return f'{self.username} -{self.role}'
+    
+    class Meta:
+
+        verbose_name='profile'
+
+        verbose_name_plural='profile'
---
+++ crm/students/utility.py
@@ -0,0 +1,34 @@
+import uuid
+from .models import Students
+
+import string
+
+import random
+
+def get_admission_number():
+
+    while True:
+         
+         pattern = str(uuid.uuid4().int)[:7]
+
+         admission_number = f'LM-{pattern}'
+
+         if not Students.objects.filter(adm_number = admission_number).exists():
+              
+              return admission_number
+
+
+
+#     print(admission_number)
+
+# get_admission_number()
+
+def get_password():
+     
+    password=''.join(random.choices(string.ascii_letters+string.digits,k=8))
+
+    print(password)
+
+    return password
+
+# get_password()
